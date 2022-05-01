e988b35f320ea7a94e43f9d6e216a3084fd59ba8
khanhnguyenvinh2002 <giakhanhvy@gmail.com>
2022-05-01 02:50:21 -0400

    update

--- clip_brighten.py
+++ clip_brighten.py
@@ -6,8 +6,8 @@ import os
 from PIL import Image, ImageDraw
 import PIL.ImageOps
 
-for i in range(1,10):
-    name = "./training/069"+str(i)+".png"
+for i in range(0,10):
+    name = "./training/085"+str(9)+".png"
     im = cv2.imread(name)
     head, tail = os.path.split(name)
     # convert the image into white and black image
@@ -21,11 +21,12 @@ for i in range(1,10):
     im = cv2.morphologyEx(im, cv2.MORPH_CLOSE, kernel)
 
     kernel = np.ones((2,2),np.uint8)
-    im = cv2.dilate(im, kernel, iterations =1) 
+    # im = cv2.dilate(im, kernel, iterations =1) 
     # make a tmpelate image for next crop
     # convert the image into white and black image
-    im[im >= 127] = 255
-    im[im < 127] = 0
+    a = 70
+    im[im >=a] = 255
+    im[im < a] = 0
 
     # kernel = np.ones((3,3),np.uint8)
     # im = cv2.dilate(im, kernel, iterations =1)
